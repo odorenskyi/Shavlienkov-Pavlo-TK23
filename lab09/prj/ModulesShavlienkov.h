0e8eb62bb261312ea921abec4596456d2f44153f
Pavel Shavlenkov <pashashavlenkov@gmail.com>
2024-03-19 18:11:39 +0200

    fixed the header file

--- lab09/prj/ModulesShavlienkov.h
+++ lab09/prj/ModulesShavlienkov.h
@@ -1,9 +1,9 @@
 #ifndef MODULESSHAVLIENKOV_H_INCLUDED
 #define MODULESSHAVLIENKOV_H_INCLUDED
 
-float s_calculation(int x, int y, int z);
-void printInfoTornado(float speed);
-void avgTemp(float arr[]);
-void countBits(int N);
+float s_calculation(int, int, int);
+void printInfoTornado(float);
+void avgTemp(float[]);
+void countBits(int);
 
 #endif // MODULESSHAVLIENKOV_H_INCLUDED
