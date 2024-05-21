d5c62ad83b488d459f4725f10688cbbf283aeac2
shavlenkov <pashashavlenkov@gmail.com>
2024-05-22 01:13:00 +0300

    done task 12.1

---
+++ lab12/prj/ModulesShavlienkov.h
@@ -0,0 +1,49 @@
+#ifndef MODULESSHAVLIENKOV_H_INCLUDED
+#define MODULESSHAVLIENKOV_H_INCLUDED
+
+#include <cmath>
+
+using namespace std;
+
+class ClassLab12_Shavlienkov {
+    private:
+        double a;
+        double b;
+
+    public:
+
+        ClassLab12_Shavlienkov(double a, double b) {
+            setA(a);
+            setB(b);
+        }
+
+        double getA() {
+            return a;
+        }
+
+        double getB() {
+            return b;
+        }
+
+        void setA(double newA) {
+            if (newA > 0) {
+                a = newA;
+            } else {
+                throw invalid_argument("FAILED");
+            }
+        }
+
+        void setB(double newB) {
+            if (newB > 0) {
+                b = newB;
+            } else {
+                 throw invalid_argument("FAILED");
+            }
+        }
+
+        double getArea() {
+            return M_PI * a * b;
+        }
+};
+
+#endif // MODULESSHAVLIENKOV_H_INCLUDED
---
+++ lab12/prj/Teacher/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+#include <sstream>
+
+#include "../ModulesShavlienkov.h"
+
+using namespace std;
+
+int main()
+{
+
+    system("chcp 65001 & cls");
+
+    int st = string(__FILE__).find("\\lab12\\prj");
+
+    if(st == -1) {
+
+        ofstream output("../../TestSuite/TestResults.txt");
+
+        for(int i = 0; i < 100; i++) {
+            Beep(900, 100);
+            Sleep(100);
+
+            output << "Встановлені вимоги порядку виконання лабораторної роботи порушено!";
+
+            output.close();
+        }
+
+    } else {
+
+        ifstream input("../../TestSuite/TestSuite.txt");
+        ofstream output("../../TestSuite/TestResults.txt");
+
+        int testCase = 1;
+
+        string line;
+
+        while (getline(input, line)) {
+            stringstream ss(line);
+            double a, b;
+
+            ss >> a;
+            ss.ignore(1, '|');
+            ss >> b;
+
+            try {
+                ClassLab12_Shavlienkov obj(a, b);
+
+                output << "TEST CASE " << testCase << endl;
+                output<< "A: " << obj.getA() << endl;
+                output << "B: " << obj.getB() << endl;
+                output << "AREA: " << obj.getArea() << endl;
+                output << "STATUS: " << "PASSED" << endl << endl;
+
+            } catch (const invalid_argument& e) {
+                output << "TEST CASE " << testCase << endl;
+                output<< "A: " << a << endl;
+                output << "B: " << b << endl;
+                output << "STATUS: " << e.what() << endl << endl;
+            }
+
+            testCase += 1;
+
+        }
+
+        input.close();
+        output.close();
+
+    }
+
+    return 0;
+}
